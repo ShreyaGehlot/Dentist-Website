e2eb0016a798ef6c469a83aabaca6f3d0b54e8ec
Shreya Gehlot <93993884+ShreyaGehlot@users.noreply.github.com>
2021-11-14 15:49:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	$('.fa-bars').click(function(){
+		$(this).toggleClass('fa-times');
+		$('.nav').toggleClass('nav-toggle');
+	});
+	$(windows).on('load scroll',function(){
+		$('.fa-bars').removeClass('fa-times');
+		$('.nav').removeClass('nav-toggle');
+	
+	
+	if($(windows).scrollTop()>10){
+		$('header').addClass('header-active');
+	}else
+	{
+		$('header').removeClass('header-active');
+	}
+	});
+});
+
